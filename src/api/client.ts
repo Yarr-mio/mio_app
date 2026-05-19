4c9fce353240603cb6a503015d4a6832e317ff65
kang-min-seok <min1042004444@gmail.com>
2026-05-19 21:54:29 +0900

    chore: axios/Tanstack query 기본 인스턴스 생성

---
+++ src/api/client.ts
@@ -0,0 +1,11 @@
+import { create } from 'axios';
+
+const apiClient = create({
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiClient;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,22 @@
 import '@/global.css';
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/layout/app-tabs';
+import queryClient from '@/api/query-client';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
