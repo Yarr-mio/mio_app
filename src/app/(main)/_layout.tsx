740c2374346808cb3cbdcd7bef0106b3abf673a7
kangminseok <min1042004444@gmail.com>
2026-05-23 23:56:55 +0900

    style: 잘못된 메인 라우팅 수정
    - 메인 라우팅이었던 체크인 페이지를 홈 페이지 하위 라우팅으로 이동
    - 메인 라우팅이었던 체크인 페이지를 더보기 페이지로 교체
    
    related to: #5

--- src/app/(main)/_layout.tsx
+++ src/app/(main)/_layout.tsx
@@ -6,8 +6,8 @@ export default function MainLayout() {
       <Tabs.Screen name="home" options={{ title: '홈' }} />
       <Tabs.Screen name="chat" options={{ title: '채팅' }} />
       <Tabs.Screen name="report" options={{ title: '별자리' }} />
-      <Tabs.Screen name="checkin" options={{ title: '기억' }} />
-      <Tabs.Screen name="my" options={{ title: '더보기' }} />
+      <Tabs.Screen name="my" options={{ title: '마이' }} />
+      <Tabs.Screen name="explore" options={{ title: '더보기' }} />
     </Tabs>
   );
 }
