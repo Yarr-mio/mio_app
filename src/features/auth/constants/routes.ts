3dd1a807d9ae702078476824181109d772f036e9
Khaneul1 <kosky1219@gmail.com>
2026-05-30 21:42:19 +0900

    chore: AUTH_ROUTES에 login, signupComplete, onboardingComplete 경로 추가
    related to: #11

--- src/features/auth/constants/routes.ts
+++ src/features/auth/constants/routes.ts
@@ -1,7 +1,10 @@
 export const AUTH_ROUTES = {
+  login: '/(auth)/login',
   termsOfService: '/(auth)/signup/termsOfService',
   signupInfo: '/(auth)/signup/info',
+  signupComplete: '/(auth)/signup/complete',
   onboardingStep1: '/(auth)/onboarding/step1Emotion',
+  onboardingComplete: '/(auth)/onboarding/onboardingComplete',
   home: '/(main)/home',
 } as const;
 
